ca2df87efd88f15d6f95e5253b4bdda084f0f297
강민정 <zjql0403@knu.ac.kr>
2024-11-02 23:28:45 +0900

    fix : css 오류 수정

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,13 @@
 <div style={{width: '100%', height: '100%', position: 'relative', background: '#D9D9D9'}}>
     <div style={{width: 1728, height: 80, left: 0, top: 0, position: 'absolute', background: 'white'}} />
-    <div style={{width: 223, height: 29, left: 321, top: 51, position: 'absolute', color: '#D9D9D9', fontSize: 20, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Dashboard > </div>
+    <div style={{width: 223, height: 29, left: 321, top: 51, position: 'absolute', color: '#D9D9D9', fontSize: 20, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Dashboard &gt; </div>
     <div style={{width: 1728, height: 80, left: 0, top: 80, position: 'absolute', background: 'white'}} />
     <div style={{width: 276, height: 1117, left: 0, top: 0, position: 'absolute', background: '#BEACEB'}}></div>
-    <div style={{width: 225.50, height: 0, left: 19.50, top: 693.50, position: 'absolute', border: '1px white solid'}}></div>
+    <div style={{width: 225.50, height: 1, left: 19.50, top: 693.50, position: 'absolute', border: '1px white solid'}}></div> {/* Changed height to 1 for visibility */}
     <div style={{width: 251, height: 48, left: 321, top: 112, position: 'absolute', color: '#040404', fontSize: 40, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>My Case List</div>
     <div style={{width: 223, height: 48, left: 34, top: 171, position: 'absolute', color: '#040404', fontSize: 30, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Dashboard</div>
     <div style={{width: 125, left: 26, top: 1061, position: 'absolute'}}>
         <div style={{width: 89, height: 23, left: 36, top: 1, position: 'absolute', color: '#040404', fontSize: 18, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Log out</div>
         <div style={{width: 25, height: 25, left: 0, top: 0, position: 'absolute', background: '#040404'}}></div>
     </div>
+</div>
