92f358989b10691b75180a2109e0bf099e274adb
Youn-Rha <ry0218@naver.com>
2024-11-03 22:42:41 +0900

    fix: SideBar 컴포넌트 합치기

---
+++ src/components/SideBarToggle.jsx
@@ -0,0 +1,183 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SideBarToggle = ({ isOpen, toggleSidebar }) => {
+  return (
+    <>
+      {isOpen && (
+        <div
+          style={{
+            width: "276px",
+            height: "100vh",
+            background: "#BEACEB",
+            position: "relative",
+            padding: "60px 0",
+          }}
+        >
+          {/* Logo and Text */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              paddingLeft: "19.5px",
+              marginBottom: "5px",
+            }}
+          >
+            <img
+              style={{ width: 50, height: 50, marginRight: 5 }}
+              src="../Grosaas_logo.png"
+              alt="Logo"
+            />
+            <div
+              style={{
+                color: "#2B00FF",
+                fontSize: 25,
+                fontFamily: "Inter",
+                fontWeight: "700",
+                wordWrap: "break-word",
+              }}
+            >
+              GroSaaS
+            </div>
+            <button
+                onClick={toggleSidebar}
+                className="text-white hover:text-indigo-200"
+              >
+                Close
+              </button>
+          </div>
+
+          {/* Dashboard Title */}
+          <div
+            style={{
+              color: "#040404",
+              fontSize: 30,
+              fontFamily: "Inter",
+              fontWeight: "700",
+              wordWrap: "break-word",
+              marginLeft: "19.5px",
+              marginTop: "5px",
+            }}
+          >
+            Dashboard
+          </div>
+
+          {/* Menu Items */}
+          <div
+            style={{
+              marginTop: "30px",
+              marginLeft: "19.5px",
+              color: "white",
+              fontSize: 20,
+              fontFamily: "Inter",
+              fontWeight: "700",
+              wordWrap: "break-word",
+              lineHeight: "2.5",
+            }}
+          >
+            <Link
+              to="/security-event"
+              style={{
+                color: "white",
+                textDecoration: "none",
+                display: "block",
+              }}
+            >
+              Security Event
+            </Link>
+            <Link
+              to="/devices-status"
+              style={{
+                color: "white",
+                textDecoration: "none",
+                display: "block",
+              }}
+            >
+              Devices Status
+            </Link>
+            <Link
+              to="/dashboard"
+              style={{
+                color: "white",
+                textDecoration: "none",
+                display: "block",
+              }}
+            >
+              My Case List
+            </Link>
+            <Link
+              to="/download-history"
+              style={{
+                color: "white",
+                textDecoration: "none",
+                display: "block",
+              }}
+            >
+              Download History
+            </Link>
+          </div>
+
+          {/* Admin Information */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              position: "absolute",
+              bottom: "60px",
+              width: "100%",
+              padding: "0 19.5px",
+            }}
+          >
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <img
+                style={{ width: 50, height: 50, marginRight: 10 }}
+                src="../usericon.png"
+                alt="User Icon"
+              />
+              <div style={{ color: "#000", fontSize: 18, fontWeight: "700" }}>
+                Admin
+              </div>
+            </div>
+            <img
+              style={{ width: 20, height: 20 }}
+              src="../settingicon.png"
+              alt="Setting Icon"
+            />
+          </div>
+
+          {/* Logout */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              position: "absolute",
+              bottom: "20px",
+              left: "19.5px",
+              color: "#000",
+              fontSize: 18,
+              fontWeight: "700",
+            }}
+          >
+            <img
+              style={{ width: 20, height: 20, marginRight: 10 }}
+              src="../logouticon.png"
+              alt="Logout Icon"
+            />
+            <div>Log out</div>
+          </div>
+        </div>
+      )}
+      {!isOpen && (
+        <button
+          onClick={toggleSidebar}
+          className="fixed top-4 left-4 z-50 text-white bg-indigo-500 p-2 rounded-md"
+        >
+          button
+        </button>
+      )}
+    </>
+  );
+};
+
+export default SideBarToggle;
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import CaseForm from "../components/Case/CaseForm";
+import SideBar from "../components/SideBar";
 
 const DashboardPage = () => {
-  return <CaseForm />;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
+  return (
+    <div className="flex">
+      <SideBar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+        <div
+          className={`flex-1 transition-all duration-300 ${
+            isSidebarOpen ? "ml-64" : "ml-0"
+          }`}
+        >
+        <CaseForm />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
