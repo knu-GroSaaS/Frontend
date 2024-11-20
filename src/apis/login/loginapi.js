14c73560fce27daa544694c6b1acaa86c463cc57
Youn-Rha <ry0218@naver.com>
2024-11-21 03:42:10 +0900

    fix: JWT 로그인 방식으로 변경 및 refreshToken api 추가

--- src/apis/login/loginapi.js
+++ src/apis/login/loginapi.js
@@ -1,5 +1,6 @@
 import axiosInstance from "../axiosInstance";
 import { useAuthStore } from "../../store";
+import Cookies from "js-cookie";
 /**
  * 로그인 요청 API
  * @param {string} username
@@ -7,14 +8,46 @@ import { useAuthStore } from "../../store";
  * @returns
  */
 export const getLogin = async (username, password) => {
-  const response = await axiosInstance.post("/login", { username, password }, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-  }
+  const response = await axiosInstance.post(
+    "/login",
+    { username, password },
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
+  console.log(response.data);
+  const { accessToken, refreshToken } = response.data;
+  const hours = 1; // 시간 설정
+  const refreshHours = 24 * 7;
+  Cookies.set("accessToken", accessToken, { expires: hours / 24, path: "/" });
+  Cookies.set("refreshToken", refreshToken, {
+    expires: refreshHours / 24,
+    path: "/",
+    secure: true,
   });
-//   console.log(response)
-//   const { accessToken } = response.data;
-//   useAuthStore.getState().setTokens(accessToken);
-//  useAuthStore.getState().setUser(response.data.user);
+  // Token 읽기
+  // const token = Cookies.get("accessToken");
+  // console.log("Access Token from Cookie:", token);
+  useAuthStore.getState().setTokens(accessToken, refreshToken);
+  return response.data;
+};
+
+/**
+ * 토큰 재발급 (POST /api/auth/refresh)
+ * @param {string} refreshToken - 재발급에 사용할 RefreshToken
+ * @returns {{ accessToken: string, refreshToken: string }}
+ */
+export const refreshTokens = async (refreshToken) => {
+  const response = await axiosInstance.post(
+    "/api/auth/refresh",
+    {},
+    {
+      headers: {
+        refreshToken,
+      },
+    }
+  );
   return response.data;
 };
