953d5b96d62baff904e2d3e7d1eb61e5db5eebd7
Widadada <jeong7571@knu.ac.kr>
2024-11-24 02:17:45 +0900

    feat: AdminPage 제작

---
+++ src/pages/AdminPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AdminForm from "../components/Admin/AdminForm";
+
+const AdminPage = () => {
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <AdminForm />
+    </div>
+  );
+};
+
+
+export default AdminPage;
