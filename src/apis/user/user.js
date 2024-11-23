31258077f9cde60b503bf73eb9d2156edc7b660c
강민정 <zjql0403@knu.ac.kr>
2024-11-23 15:23:55 +0900

    fix : password change api 연결

--- src/apis/user/user.js
+++ src/apis/user/user.js
@@ -2,18 +2,28 @@ import axiosInstance from "../axiosInstance";
 
 /**
  * 비밀번호 변경
+ * @param {string} username - 사용자 아이디
  * @param {string} currentPassword - 현재 비밀번호
  * @param {string} newPassword - 새로운 비밀번호
  * @returns {Promise<void>}
  */
-export const changePassword = async (currentPassword, newPassword) => {
-  await axiosInstance.put(
-    "/user/changepassword",
-    { currentPassword, newPassword },
-    {
-      headers: {
-        "Content-Type": "application/json",
+export const changePassword = async (username, currentPassword, newPassword) => {
+  try {
+    await axiosInstance.post(
+      "/password/update",
+      {
+        username,
+        currentPassword,
+        newPassword,
       },
-    }
-  );
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.error("Failed to update password:", error);
+    throw error;
+  }
 };
