27ffa87e6575d2ebba9dc4f3f9bb1e2fd78fc34c
Widadada <jeong7571@knu.ac.kr>
2024-11-24 12:55:24 +0900

    fix: admin 권한

--- src/apis/admin/admin.js
+++ src/apis/admin/admin.js
@@ -1,11 +1,25 @@
 import axiosInstance from "../axiosInstance";
-
+import Cookies from "js-cookie";
+import { refreshTokens } from "../login/loginapi";
 /**
  * 가입 유저 정보 조회
  * @returns
  */
+// export const getUser = async () => {
+//   const response = await axiosInstance.get("/manager", {
+//   });
+//   return response.data;
+// };
+
 export const getUser = async () => {
+  const token = Cookies.get("accessToken");  // 쿠키에서 토큰 가져오기
+  console.log("Access Token:", token);  // 토큰이 정상적으로 출력되는지 확인
+
   const response = await axiosInstance.get("/manager", {
+    headers: {
+      Authorization: `Bearer ${token}`,  // Bearer 토큰 방식으로 전달
+    },
+    withCredentials: true,
   });
   return response.data;
 };
