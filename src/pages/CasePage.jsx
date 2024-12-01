bc33da0853065e9d68535c4e3a8cffce10d0a33e
Youn-Rha <ry0218@naver.com>
2024-12-02 03:05:48 +0900

    fix: sidebar 수정

--- src/pages/CasePage.jsx
+++ src/pages/CasePage.jsx
@@ -5,16 +5,10 @@ import SideBar from "../components/SideBar";
 const CasePage = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
-  return(
+  return (
     <div className="flex">
-      <SideBar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <div
-          className={`flex-1 transition-all duration-300 ${
-            isSidebarOpen ? "ml-64" : "ml-0"
-          }`}
-        >
-        <CaseUnit />
-      </div>
+      <SideBar />
+      <CaseUnit />
     </div>
   );
 };
