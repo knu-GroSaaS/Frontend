43c8be490db07feed1e2fdffc40efaadab8d018e
Youn-Rha <ry0218@naver.com>
2024-12-03 01:46:03 +0900

    fix: 창 끌 때 로그아웃 로직 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,10 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { useAuthStore } from "./store";
-import { logOut } from './apis/login/loginapi.js';
-import Cookies from 'js-cookie';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-function Root() {
-  const clearAuth = useAuthStore((state) => state.clearAuth);
-
-  useEffect(() => {
-    const handleBeforeUnload = () => {
-      const refreshToken = Cookies.get("refreshToken");
-      clearAuth();
-      logOut(refreshToken);
-    };
-
-    window.addEventListener('beforeunload', handleBeforeUnload);
-    return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
-    };
-  }, [clearAuth]);
-
-  return <App />;
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>
 );
