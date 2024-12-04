8b7b97700741489b867f562f41fb3e71a09db47e
강민정 <zjql0403@knu.ac.kr>
2024-12-05 00:48:15 +0900

    feat : 이메일 인증 함수

--- src/apis/user/user.js
+++ src/apis/user/user.js
@@ -24,10 +24,32 @@ export const changePassword = (username, currentPassword, newPassword) => {
 };
 
 /**
- * GET /manager/getuser
  * 사용자 정보를 가져오는 API
+ * GET /manager/getuser
  * @returns {Promise<Object>} 사용자 정보 객체 반환
  */
 export const getUser = () => {
   return axiosInstance.get("/getuser");
 };
+
+/**
+ * 인증 코드 전송 API
+ * POST /password
+ * 이메일로 인증 코드를 전송하는 API
+ * @param {string} email - 사용자 이메일 주소
+ * @returns {Promise<string>} 성공 시 인증 코드 반환
+ */
+export const sendVerificationCode = (email) => {
+  return axiosInstance.post(
+    "/password",
+    { email },
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`, // 인증 토큰 포함
+        "Content-Type": "application/json",
+      },
+    }
+  );
+};
+
+
